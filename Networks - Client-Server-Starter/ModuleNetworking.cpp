7ce7cb8caf590328eae2c8cc2faca4700bcb0f05
Marqq <36154480+Marcgs96@users.noreply.github.com>
2020-10-27 17:51:29 +0100

    Exercise 3

---
+++ Networks - Client-Server-Starter/ModuleNetworking.cpp
@@ -0,0 +1,173 @@
+#include "Networks.h"
+#include "ModuleNetworking.h"
+#include <list>
+
+
+static uint8 NumModulesUsingWinsock = 0;
+
+
+
+void ModuleNetworking::reportError(const char* inOperationDesc)
+{
+	LPVOID lpMsgBuf;
+	DWORD errorNum = WSAGetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		errorNum,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	ELOG("Error %s: %d- %s", inOperationDesc, errorNum, lpMsgBuf);
+}
+
+void ModuleNetworking::disconnect()
+{
+	for (SOCKET socket : sockets)
+	{
+		shutdown(socket, 2);
+		closesocket(socket);
+	}
+
+	sockets.clear();
+}
+
+bool ModuleNetworking::init()
+{
+	if (NumModulesUsingWinsock == 0)
+	{
+		NumModulesUsingWinsock++;
+
+		WORD version = MAKEWORD(2, 2);
+		WSADATA data;
+		if (WSAStartup(version, &data) != 0)
+		{
+			reportError("ModuleNetworking::init() - WSAStartup");
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool ModuleNetworking::preUpdate()
+{
+	if (sockets.empty()) return true;
+
+	// NOTE(jesus): You can use this temporary buffer to store data from recv()
+	const uint32 incomingDataBufferSize = Kilobytes(1);
+	byte incomingDataBuffer[incomingDataBufferSize];
+
+	// TODO(jesus): select those sockets that have a read operation available
+	fd_set readfdset;
+	FD_ZERO(&readfdset);
+
+	for (auto s : sockets)
+	{
+		FD_SET(s, &readfdset);
+	}
+
+	timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0;
+
+	int result = select(0, &readfdset, nullptr, nullptr, &timeout);
+	if (result == SOCKET_ERROR) {
+		reportError("Error selecting readable sockets");
+	}
+
+	// TODO(jesus): for those sockets selected, check wheter or not they are
+	// a listen socket or a standard socket and perform the corresponding
+	// operation (accept() an incoming connection or recv() incoming data,
+	// respectively).
+	// On accept() success, communicate the new connected socket to the
+	// subclass (use the callback onSocketConnected()), and add the new
+	// connected socket to the managed list of sockets.
+	// On recv() success, communicate the incoming data received to the
+	// subclass (use the callback onSocketReceivedData()).
+
+	std::list<SOCKET> disconnectedSockets;
+
+	for (SOCKET s : sockets) {
+		if (FD_ISSET(s, &readfdset)) {
+			if (isListenSocket(s)) {
+				//Remote Address creation
+				sockaddr_in connectionAddress;
+				connectionAddress.sin_family = AF_INET; // IPv4
+				connectionAddress.sin_addr.S_un.S_addr = INADDR_ANY; // Any local IP address
+
+				result = accept(s, (struct sockaddr*)&connectionAddress, (socklen_t*)&connectionAddress);
+				if (result > 0)
+					onSocketConnected(s, connectionAddress);
+				else {
+					reportError("Error creating connection socket");
+				}
+			}
+			else {
+				result = recv(s, (char*)incomingDataBuffer, incomingDataBufferSize, 0);
+				if (result != SOCKET_ERROR) {
+					incomingDataBuffer[result] = '\0';
+					onSocketReceivedData(s, incomingDataBuffer);
+				}
+				else if(result == 0 || result == ECONNRESET){
+					shutdown(s, 2);
+					closesocket(s);
+					disconnectedSockets.push_back(s);
+					onSocketDisconnected(s);
+				}
+				else {
+					reportError("Error receiving message");
+				}
+			}
+		}
+	}
+
+
+	// TODO(jesus): handle disconnections. Remember that a socket has been
+	// disconnected from its remote end either when recv() returned 0,
+	// or when it generated some errors such as ECONNRESET.
+	// Communicate detected disconnections to the subclass using the callback
+	// onSocketDisconnected().
+
+
+	for (std::vector<SOCKET>::iterator it = sockets.begin(); it != sockets.end();) {
+		if (std::find(disconnectedSockets.begin(), disconnectedSockets.end(), (*it)) != disconnectedSockets.end()) {
+			it = sockets.erase(it);
+		}
+		else {
+			++it;
+		}
+	}
+
+	// TODO(jesus): Finally, remove all disconnected sockets from the list
+	// of managed sockets.
+
+	return true;
+}
+
+bool ModuleNetworking::cleanUp()
+{
+	disconnect();
+
+	NumModulesUsingWinsock--;
+	if (NumModulesUsingWinsock == 0)
+	{
+
+		if (WSACleanup() != 0)
+		{
+			reportError("ModuleNetworking::cleanUp() - WSACleanup");
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void ModuleNetworking::addSocket(SOCKET socket)
+{
+	sockets.push_back(socket);
+}
---
+++ Networks - Client-Server-Starter/ModuleNetworkingClient.cpp
@@ -0,0 +1,89 @@
+#include "ModuleNetworkingClient.h"
+
+
+bool  ModuleNetworkingClient::start(const char * serverAddressStr, int serverPort, const char *pplayerName)
+{
+	playerName = pplayerName;
+
+	// TODO(jesus): TCP connection stuff
+	// - Create the socket
+	// - Create the remote address object
+	// - Connect to the remote address
+	// - Add the created socket to the managed list of sockets using addSocket()
+
+	// If everything was ok... change the state
+
+	connectSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	//Server address creation
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(serverPort);
+	inet_pton(AF_INET, serverAddressStr, &serverAddress.sin_addr);
+
+
+
+	int result = connect(connectSocket, (const struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+	if (result != SOCKET_ERROR) {
+		addSocket(connectSocket);
+		state = ClientState::Start;
+	}
+	else {
+		reportError("Error connecting the client socket to the remote adress.");
+	}
+
+	return true;
+}
+
+bool ModuleNetworkingClient::isRunning() const
+{
+	return state != ClientState::Stopped;
+}
+
+bool ModuleNetworkingClient::update()
+{
+	if (state == ClientState::Start)
+	{
+		// TODO(jesus): Send the player name to the server
+		int result = send(connectSocket, playerName.c_str(), playerName.size(), 0);
+		if (result != SOCKET_ERROR) {
+			state = ClientState::Logging;
+		}
+		else {
+			reportError("Client error sending message");
+		}
+
+	}
+
+	return true;
+}
+
+bool ModuleNetworkingClient::gui()
+{
+	if (state != ClientState::Stopped)
+	{
+		// NOTE(jesus): You can put ImGui code here for debugging purposes
+		ImGui::Begin("Client Window");
+
+		Texture *tex = App->modResources->client;
+		ImVec2 texSize(400.0f, 400.0f * tex->height / tex->width);
+		ImGui::Image(tex->shaderResource, texSize);
+
+		ImGui::Text("%s connected to the server...", playerName.c_str());
+
+		ImGui::End();
+	}
+
+	return true;
+}
+
+void ModuleNetworkingClient::onSocketReceivedData(SOCKET socket, byte * data)
+{
+	state = ClientState::Stopped;
+}
+
+void ModuleNetworkingClient::onSocketDisconnected(SOCKET socket)
+{
+	state = ClientState::Stopped;
+}
+

4bd80d273fef41cfc33b94af12609570d7fecf3c
AxelAlavedra <axel.alavedra@gmail.com>
2020-12-29 20:03:00 +0100

    Starting bow spell

--- Multiplayer Game/ModuleBehaviour.h
+++ Multiplayer Game/ModuleBehaviour.h
@@ -8,14 +8,15 @@ public:
 
 	bool update() override;
 
-	Behaviour * addBehaviour(BehaviourType behaviourType, GameObject *parentGameObject);
-	Player * addPlayer(GameObject *parentGameObject);
-	Projectile * addProjectile(BehaviourType behaviourType, GameObject *parentGameObject);
+	Behaviour* addBehaviour(BehaviourType behaviourType, GameObject *parentGameObject);
+	Player* addPlayer(GameObject *parentGameObject);
+	Projectile* addProjectile(BehaviourType behaviourType, GameObject *parentGameObject);
 	DeathGhost* addDeathGhost(GameObject* parentGameObject);
 	Weapon* addWeapon(GameObject* parentGameObject);
 	Spell* addSpell(BehaviourType behaviourType, GameObject* parentGameObject);
 
 	std::list<Player> GetPlayersList();
+
 private:
 
 	void handleBehaviourLifeCycle(Behaviour * behaviour);
